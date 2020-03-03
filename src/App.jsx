17866b1d9955d8c1db1091d8404db661862618ba
Yuxing Jiang <yjian118@uottawa.ca>
2020-03-03 12:35:56 -0500

    add firebase reference

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './App.css';
+import { db } from './Firebase';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+        </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
